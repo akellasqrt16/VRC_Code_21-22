6b75db008a3eb845124e2896f8e3688db48905b1
akellasqrt16 <64755067+akellasqrt16@users.noreply.github.com>
2022-02-22 15:02:58 -0800

    Update main.cpp

--- TestingCode/src/main.cpp
+++ TestingCode/src/main.cpp
@@ -15,7 +15,6 @@
 
 // Import Statements
 #include "vex.h"
-
 #include <string>
 
 //Field definition
@@ -23,9 +22,12 @@ using namespace vex;
 
 vex::competition    Competition;
 
-bool aPressed = false;
+bool reverseMode = false;
+bool platformMode = false;
 int deadzone = 15;
 bool pEx = false;
+int axis3 = 0;
+int axis1 = 0;
 
 int counter = 0;
 
@@ -46,7 +48,9 @@ int main() {
   
   // infinite loop for the controller binds
   while (1) {  
+    
 
+    //Pnuematics code
     if (Yeetroller.ButtonA.pressing()){
       counter++;
       if (counter == 1 ){
@@ -54,17 +58,61 @@ int main() {
         p1.set(pEx);
         p2.set(pEx);
       }
-      
+      this_thread::sleep_for(200);
     }
   
-    //DRIVETRAIN CALCULATIONS
+    /*
+    * DRIVETRAIN CALCULATIONS
+    */
+
+    //getting controller values
+    axis3 = Yeetroller.Axis3.position();
+    axis1 = Yeetroller.Axis1.position();
 
 
-    //measuring values
+
+
+    //reverse mode calculations
+    if (Yeetroller.ButtonX.pressing()){
+      reverseMode = !reverseMode;
+      if (reverseMode){
+        printTextCont("Reverse Mode: ON", 1);
+      } else {
+        printTextCont("Reverse Mode: OFF", 1);
+      }
+      this_thread::sleep_for(400);
+    }
+    if (reverseMode){
+      axis3 = -axis3;
+    }
+
+
+
+
+    //platform mode
+    if (Yeetroller.ButtonY.pressing()){
+      platformMode = !platformMode;
+      if (platformMode){
+        printTextCont("Platform Mode: ON", 10);
+      } else {
+        printTextCont("Platform Mode: OFF", 10);
+      }
+      this_thread::sleep_for(400);
+    }
+    
+
+  
+
+    /*
+    * calculating speed
+    */
     // Get the velocity percentage of the left motor. (Axis3 + Axis1)
-    int leftMotorSpeed =  Yeetroller.Axis3.position() + Yeetroller.Axis1.position();
+    int leftMotorSpeed =  axis3 + axis1;
     // Get the velocity percentage of the right motor. (Axis3 - Axis1)
-    int rightMotorSpeed = Yeetroller.Axis3.position() - Yeetroller.Axis1.position();
+    int rightMotorSpeed = axis3 - axis1;
+
+
+
 
     //Deadzone calculations
     if (abs(leftMotorSpeed) < deadzone){
@@ -73,18 +121,33 @@ int main() {
     if (abs(rightMotorSpeed) < deadzone){
       rightMotorSpeed = 0;
     }
-    
+
+
 
 
     //DRIVETRAIN CODE
 
+    if (platformMode){
+      if (leftMotorSpeed == 0 && rightMotorSpeed == 0){
+        leftDriveMotor.stop(vex::brakeType::hold);
+        rightDriveMotor.stop(vex::brakeType::hold);
+        leftDriveMotor2.stop(vex::brakeType::hold);
+        rightDriveMotor2.stop(vex::brakeType::hold);
 
-    leftDriveMotor.spin(vex::directionType::fwd, leftMotorSpeed, vex::velocityUnits::pct);
-    leftDriveMotor2.spin(vex::directionType::fwd, leftMotorSpeed, vex::velocityUnits::pct);
-   
+      } else {
+        leftDriveMotor.spin(vex::directionType::fwd, leftMotorSpeed, vex::velocityUnits::pct);
+        leftDriveMotor2.spin(vex::directionType::fwd, leftMotorSpeed, vex::velocityUnits::pct);
+        rightDriveMotor.spin(vex::directionType::fwd, rightMotorSpeed, vex::velocityUnits::pct);
+        rightDriveMotor2.spin(vex::directionType::fwd, rightMotorSpeed, vex::velocityUnits::pct);
+      }
+    } else {
+      leftDriveMotor.spin(vex::directionType::fwd, leftMotorSpeed, vex::velocityUnits::pct);
+      leftDriveMotor2.spin(vex::directionType::fwd, leftMotorSpeed, vex::velocityUnits::pct);
+    
 
-    rightDriveMotor.spin(vex::directionType::fwd, rightMotorSpeed, vex::velocityUnits::pct);
-    rightDriveMotor2.spin(vex::directionType::fwd, rightMotorSpeed, vex::velocityUnits::pct);
+      rightDriveMotor.spin(vex::directionType::fwd, rightMotorSpeed, vex::velocityUnits::pct);
+      rightDriveMotor2.spin(vex::directionType::fwd, rightMotorSpeed, vex::velocityUnits::pct);
+    }
     
 
 
